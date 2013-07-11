4630bcf6378bd5dfdd49f245a7b9011ea6340579
Steve Klise <sk@skli.se>
2013-07-11 11:22:58 -0400

    Grabbing the first h2 element

--- htmlbook.js
+++ htmlbook.js
@@ -1,16 +1,33 @@
 htmlbook = function (source) {
-  // source in this case is a string.
-  body = $('<body>').append(source)
-
-  var children = body.children();
+  // Source should be a string of html. Put it in a <body> element to enable traversing with jQuery.
+  var body = $('<body>').append(source);
+  children = body.children();
 
   // the first child must be an h1
-  var first_element = $(body.children().first())
+  first_element = $(body.children().first())
 
-  if (children[0].tagName == "H1") {
-    chapter = {title: first_element.html(), content: children.first().nextUntil('h2')}
-    return chapter;
-  } else {
+  // Exit with an error if the first element is not an H1.
+  if (first_element.prop("tagName") != "H1") {
     return "Error, the first child must be an H1";
   }
+
+  // rename the first element
+  h1 = first_element;
+  h1_children = h1.nextUntil('h1');
+  chapter = $('<section data-type="chapter">').append(h1);
+
+  // Ok, so we are inside an H1 block right now, let's look for h2's
+  before_h2 = $(h1_children).first().nextUntil('h2');
+  // before_h2 is all elements until the next h2, so our h2 is one more than the length of before_h2
+  h2 = $(h1_children[before_h2.length+1])
+
+  sect1_body = h2.nextUntil('h2');
+
+  sect1 = $('<section data-type="sect1">').append(h2);
+  sect1.append(sect1_body);
+
+  chapter.append(before_h2);
+  chapter.append(sect1);
+
+  return chapter;
 }
