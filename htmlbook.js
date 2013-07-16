c39219174579d87c1e8878529ec104276e942d73
Steve Klise <sk@skli.se>
2013-07-16 17:04:01 -0400

    optimizing code

--- htmlbook.js
+++ htmlbook.js
@@ -49,23 +49,23 @@ htmlbook = function (source) {
   var children = body.children();
   var htmlbooklevel = htmlbook_spec.chapter;
 
-  return make_section(children, htmlbooklevel);
+  return parse_html(children, htmlbooklevel);
 
-  // make_section - recursive part of this library, takes an amount of html,
+  // parse_html - recursive part of this library, takes an amount of html,
   // looks at top level header, and looks for a sub-section. if there's a
   // subsection, call this again.
   // content: jQuery collection where the first element is a header.
-  function make_section (content, htmlbooklevel) {
+  function parse_html (content, htmlbooklevel) {
+    console.log("\n>>> Making section " + htmlbooklevel.name);
     // initialize variables
-    var section_content;
-    var after_stuff = '';
+    var section;
+    var next_section = '';
 
     // Take the input, wrap it in a div and then get the children. In this way
     // if 'content' is a string it will be converted into a jQuery object and
     // if it's already a jQuery object everything is fine.
     var wrap = $('<div>').html(content);
     var children = wrap.children();
-
 
     // Do some analysis on the first element. HTMLBook is pretty strict as to
     // what the first element should be: either a heading or a div must start
@@ -77,35 +77,46 @@ htmlbook = function (source) {
     if (first_element != false) {
       console.log("found a heading as the first element")
       var more_headings = find_headings(content, first_element.tag_name);
-
-      if (more_headings == 'samelevel') {
-        var subheading = htmlbook_spec[htmlbooklevel.child];
-        var next_heading = wrap.find(first_element.tag_name+':gt(0)').first();
-
-        var nested_element_count = next_heading.index() - 1;
-
-        section_content = make_section(content.splice(1, nested_element_count), subheading);
-        after_stuff = make_section(content.splice(1), htmlbooklevel);
-      } else if (more_headings == 'subheadings') {
-        var subheading = htmlbook_spec[htmlbooklevel.child];
-        console.log('sub headings', subheading);
-        section_content = make_section(content.splice(1), subheading);
-      } else {
-        section_content = content.splice(1);
-      }
-
-      var section = $('<section data-type="' + htmlbooklevel.name + '">');
-      section.append(first_element.html).append(section_content);
+      var arr = wrap_in_section(htmlbooklevel, children, first_element, more_headings);
+      section = arr[0];
+      next_section = arr[1];
     } else {
       var rest = content.splice(1);
       var first = content;
-      console.log('fail point', first, content)
-      section_content = $('<div>').html(first).html() + make_section(rest, htmlbooklevel);
+      var section_content;
+      console.log('no more headings in this branch.');
+      section_content = $('<div>').html(first).html() + parse_html(rest, htmlbooklevel);
 
-      var section = $('<div>').html(section_content).html();
+      section = $('<div>').html(section_content).html();
     }
 
-    return $('<div>').html(section).html() + after_stuff;
+    return $('<div>').html(section).html() + next_section;
+  }
+
+  function wrap_in_section (htmlbooklevel, children, first_element, more_headings) {
+    var subheading, next_heading_index, nested_element_count, section_content, next_section = ''
+
+    if (more_headings == 'samelevel') {
+      subheading = htmlbook_spec[htmlbooklevel.child];
+      next_heading_index = $('<div>').html(children).find(first_element.tag_name+':gt(0)').first().index();
+      nested_element_count = next_heading_index - 1;
+
+      section_content = parse_html(children.splice(1, nested_element_count), subheading);
+      next_section = parse_html(children.splice(1), htmlbooklevel);
+    } else if (more_headings == 'subheadings') {
+      subheading = htmlbook_spec[htmlbooklevel.child];
+      console.log('sub headings');
+      section_content = parse_html(children.splice(1), subheading);
+    } else {
+      // There are no other headings, so the work is done.
+      console.log("no more headings in the document.");
+      section_content = children.splice(1);
+    }
+
+    section = $('<section data-type="' + htmlbooklevel.name + '">');
+    section.append(first_element.html).append(section_content);
+
+    return [section, next_section];
   }
 
   function find_headings(content, parent_tag_name) {
@@ -117,7 +128,6 @@ htmlbook = function (source) {
     if (heading_index == 6) {
       return false;
     } else if ($(wrap).find(parent_tag_name).length > 0) {
-      console.log('found same level', parent_tag_name, $(wrap).find(parent_tag_name))
       return 'samelevel';
     } else if (subheadings.length > 0) {
       return 'subheadings';
