2c68a445fdde9d6e207c9aae4eb68e75af7ea264
Steve Klise <sk@skli.se>
2013-07-16 17:52:09 -0400

    Semicolons etc

--- test/spec/htmlbook_spec.js
+++ test/spec/htmlbook_spec.js
@@ -1,12 +1,11 @@
 // Check to see if one level of the parser is working.
 describe('#0 - One heading, two paragraphs', function () {
-  var source_html;
-  var spec_html;
+  var source_html, spec_html;
 
   beforeEach(function () {
     source_html = $('#example0').html();
     spec_html = $('#example0-spec').html().split("\n").join('');
-  })
+  });
 
   it("should convert to be equal to the sample", function () {
     var parsed = htmlbook($('<div>').html(source_html).html());
@@ -16,13 +15,12 @@ describe('#0 - One heading, two paragraphs', function () {
 
 // Nested but a single branch.
 describe('#1 - Three Headings One Heading Per Level', function () {
-  var source_html;
-  var spec_html;
+  var source_html, spec_html;
 
   beforeEach(function () {
     source_html = $('#example1').html();
     spec_html = $('#example1-spec').html().split("\n").join('');
-  })
+  });
 
   it("should convert to be equal to the sample", function () {
     var parsed = htmlbook($('<div>').html(source_html).html());
@@ -31,14 +29,13 @@ describe('#1 - Three Headings One Heading Per Level', function () {
 });
 
 describe('#2 - Two Headings Per Level, each with child content.', function () {
-  var source_html;
-  var spec_html;
+  var source_html, spec_html;
 
   beforeEach(function () {
     console.log("#########\nTEST #2\n#########")
     source_html = $('#example2').html();
     spec_html = $('#example2-spec').html().split("\n").join('');
-  })
+  });
 
   it("should convert to be equal to the sample", function () {
     var parsed = htmlbook($('<div>').html(source_html).html());
@@ -47,14 +44,13 @@ describe('#2 - Two Headings Per Level, each with child content.', function () {
 });
 
 describe('#3 - Two Headings Per Level, each with child content.', function () {
-  var source_html;
-  var spec_html;
+  var source_html, spec_html;
 
   beforeEach(function () {
     console.log("#########\nTEST #3\n#########")
     source_html = $('#example3').html();
     spec_html = $('#example3-spec').html().split("\n").join('');
-  })
+  });
 
   it("should convert to be equal to the sample", function () {
     var parsed = htmlbook($('<div>').html(source_html).html());
@@ -63,14 +59,13 @@ describe('#3 - Two Headings Per Level, each with child content.', function () {
 });
 
 describe('#4 - Parser should accept both strings and jQuery objects.', function () {
-  var source_html;
-  var spec_html;
+  var source_html, spec_html;
 
   beforeEach(function () {
-    console.log("#########\nTEST #4\n#########")
+    console.log("#########\nTEST #4\n#########");
     source_html = $('#example3').html();
     spec_html = $('#example3-spec').html().split("\n").join('');
-  })
+  });
 
   it("should convert to be equal to the sample", function () {
     var parsed = htmlbook($('<div>').html(source_html).children());
