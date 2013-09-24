4e633fc236f612f21cb8e9f52816999bbfa8b8e5
Steve Klise <sklise@oreilly.com>
2013-09-24 19:23:26 -0400

    Find element name in schema throughout tree

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var sys = require('sys'),
   fs = require('fs'),
   _ = require('underscore'),
-  htmlparser = require('htmlparser2')
+  htmlparser = require('htmlparser2'),
+  xml2js = require('xml2js'),
+  xml_parser = new xml2js.Parser(),
+  schema,
+  elements;
 
 var existy = function (x) {
   return x != null;
@@ -32,7 +36,7 @@ traverse = function (dom_tree, depth) {
 
   _.forEach(dom_tree, function (node) {
     if (existy(node.name))
-      console.log(indentation(depth) + node.name);
+      console.log(indentation(depth),  _.findWhere(elements, {name: node.name}));
     if (existy(node.children))
       traverse(node.children, depth + 1)
   });
@@ -42,6 +46,22 @@ htmlbook = function (input) {
   parse(input, traverse);
 }
 
-fs.readFile("../htmlbook/samples/htmlbook.html", "utf-8", function (e,d) {
-  htmlbook(d)
-})
+// When parsing finishes
+xml_parser.addListener('end', function(result) {
+  // save the result
+  schema = result;
+
+  elements = _.map(schema["xs:schema"]["xs:element"], function (x) {
+    return x["$"]
+  });
+
+  // Read the source, start conversion
+  fs.readFile("../htmlbook/samples/htmlbook.html", "utf-8", function (e,d) {
+    htmlbook(d);
+  });
+});
+
+// Start by parsing schema
+fs.readFile("../HTMLBook/schema/htmlbook.xsd", function(err, data) {
+  xml_parser.parseString(data);
+});
